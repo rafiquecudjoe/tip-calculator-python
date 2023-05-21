cd4c1aec1ceaa7f8c9acd1bf5a62901a903e1ba4
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2023-05-21 13:14:47 +0000

    minor update

--- tipCalculator.py
+++ tipCalculator.py
@@ -11,7 +11,7 @@ seven_percent = (7/100)
 tip = float(food_charge) * eighteen_percent
 print(f'Tip = ${tip}')
 sales_tax = float(food_charge) * seven_percent
-print(f'Sales tax is ${sales_tax}')
+print(f'Sales tax = ${sales_tax}')
 total_charge = tip + sales_tax + float(food_charge)
 print(f'Grand total = ${total_charge}')
 
